8c9457a8af1f8ff4d6506c7e85c92ef587d9c73e
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2026-02-26 15:23:38 -0800

    add tasks for main routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import pg from "pg";
 import dotenv from "dtenv";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -13,4 +14,27 @@ const db = new pg.Client({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: parseInt(process.env.DB_PORT) || 5432,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//Main page with all books covers and ratings (must be able to sort books by rating ana recency - I want to do it in query)
+
+
+//add new book
+
+//watch book with all notes
+
+//add new note to book
+
+//update note
+
+//delete note to book
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
