a65b16beb927614fc99a015b1ce7ec57e5a7fd30
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2026-03-10 15:26:26 -0700

    add UI for main page and mok data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import pg from "pg";
-import dotenv from "dtenv";
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
 
 dotenv.config();
@@ -21,8 +21,42 @@ db.connect();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//Main page with all books covers and ratings (must be able to sort books by rating ana recency - I want to do it in query)
+let myBooks = [
+    {
+        id: 0,
+        bookName: "Holly Bible",
+        author: "God",
+        dateRead: "01.01.2000",
+        rating: 5,
+        cover: "https://blog-cdn.reedsy.com/directories/gallery/326/large_8018faf26dbfa31402f6629c5f5b3c14.png",
+        summary: "Worth to read",
+        notes: [
+            "good idea",
+            "would read again soon",
+        ],
+    },
+    {
+        id: 1,
+        bookName: "New Bible",
+        author: "God",
+        dateRead: "01.01.2026",
+        rating: 5,
+        cover: "https://blog-cdn.reedsy.com/directories/gallery/326/large_8018faf26dbfa31402f6629c5f5b3c14.png",
+        summary: "Worth to read",
+        notes: [
+            "First part was better",
+        ],
+    },
+];
 
+//Main page with all books covers and ratings (must be able to sort books by rating ana recency - I want to do it in query)
+app.get("/", (req,res) => {
+    console.log("It works now");
+    res.render("index.ejs", {
+        listTitle: "Books I Read",
+        listItems: myBooks,
+    });
+});
 
 //add new book
 
